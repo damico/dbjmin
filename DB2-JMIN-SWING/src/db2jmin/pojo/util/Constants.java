ba552cc07877c32e8e4fb3c26ff4859e643c3b65
jd.comment <jd.comment@1d0d8012-f3cf-11dd-802b-177316e4ac06>
2009-02-05 22:45:20 +0000

    Full Import of 0.2 version to Google Code. For history, check SourceForge (our old repository http://sourceforge.net/projects/db2-jmin/) Continuation...

---
+++ DB2-JMIN-SWING/src/db2jmin/pojo/util/Constants.java
@@ -0,0 +1,43 @@
+/* ******************************************************************* 	*/
+/* DB2-JMIN / MULTI-JMIN (MULTI DB CLIENT)								*/
+/* Copyright (C) 2007  Jose' Ricardo de Oliveira Damico					*/
+/* damico@dcon.com.br													*/
+/* 																		*/
+/* This program is free software; you can redistribute it and/or		*/
+/* modify it under the terms of the GNU General Public License			*/
+/* as published by the Free Software Foundation; either version 2		*/
+/* of the License, or (at your option) any later version.				*/
+/* 																		*/
+/* This program is distributed in the hope that it will be useful,		*/
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of		*/
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the		*/
+/* GNU General Public License for more details.							*/
+/* 																		*/
+/* You should have received a copy of the GNU General Public License	*/
+/* along with this program; if not, write to the Free Software			*/
+/* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 			*/
+/* MA  02110-1301, USA.													*/
+/* ******************************************************************** */
+
+package db2jmin.pojo.util;
+
+/**
+ * This class intents to hold all application
+ * Constants
+ * @author Jose Damico (damico@dcon.com.br)
+ * */
+
+public class Constants {
+
+	public static final String APPNAME = "DBJMIN - Desktop - 0.2 (2008-may-12)";
+	public static final int LOGTEXTH = 80;
+	public static final int SQLTEXTH = 80;
+	public static final String PRESERVER_FILE = "preServers.xml";
+	public static final int DEFAULT_OS_SPEC_H = 2;
+	public static final String LOGNAME = "DBJMIN";
+	public static final boolean VERBOSE_CONSOLE = false;
+	public static final String APPLOGO = "dbjmin.png";
+	public static int frameWidth = 798;
+	public static int frameHeight = 50;
+
+}
---
+++ DB2-JMIN-SWING/src/db2jmin/pojo/util/InputDataValidation.java
@@ -0,0 +1,198 @@
+/* ******************************************************************* 	*/
+/* DB2-JMIN / MULTI-JMIN (MULTI DB CLIENT)								*/
+/* Copyright (C) 2007  Jose' Ricardo de Oliveira Damico					*/
+/* damico@dcon.com.br													*/
+/* 																		*/
+/* This program is free software; you can redistribute it and/or		*/
+/* modify it under the terms of the GNU General Public License			*/
+/* as published by the Free Software Foundation; either version 2		*/
+/* of the License, or (at your option) any later version.				*/
+/* 																		*/
+/* This program is distributed in the hope that it will be useful,		*/
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of		*/
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the		*/
+/* GNU General Public License for more details.							*/
+/* 																		*/
+/* You should have received a copy of the GNU General Public License	*/
+/* along with this program; if not, write to the Free Software			*/
+/* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 			*/
+/* MA  02110-1301, USA.													*/
+/* ******************************************************************** */
+
+package db2jmin.pojo.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+import db2jmin.pojo.data.DBconnector;
+
+/**
+ * This class loads is used to Validade input data from user, as db, host, port,
+ * dbname etc
+ * 
+ * @author Jose Damico (damico@dcon.com.br)
+ */
+
+public class InputDataValidation {
+	public InputDataValidation() {
+		log.AddLogLine("Invoking  InputDataValidation()");
+	}
+
+	public ArrayList formValidationDerbye(String form_name, ArrayList form_data) {
+
+		ArrayList validationResult = new ArrayList();
+		boolean ret = true;
+		validationResult.add(0, ret);
+
+		if (form_data.get(2).toString().equals("")) {
+			ret = false;
+			validationResult.add("Wrong database name");
+			log.AddLogLine("EXCEPTION: " + form_data.get(2).toString());
+		}
+
+		if (ret) {
+
+			DBconnector dbc = new DBconnector(form_data);
+			ret = dbc.testConn();
+		}
+
+		return validationResult;
+	}
+
+	public ArrayList<String> testTextFields(ArrayList<String> form_data){
+		ArrayList<String> validationResult = new ArrayList<String>();
+		if (form_data.get(0).toString().equals("")) {
+			validationResult.add("Wrong server address");
+			log.AddLogLine("EXCEPTION: " + form_data.get(0).toString());
+		}
+		if (form_data.get(1).toString().equals("")) {
+			validationResult.add("Wrong server port");
+			log.AddLogLine("EXCEPTION: " + form_data.get(1).toString());
+		}
+
+		if (form_data.get(2).toString().equals("")) {
+			validationResult.add("Wrong database name");
+			log.AddLogLine("EXCEPTION: " + form_data.get(2).toString());
+		}
+		
+		if (form_data.get(3).toString().equals("")) {
+			validationResult.add("Wrong user name");
+			log.AddLogLine("EXCEPTION: " + form_data.get(3).toString());
+		}
+		return validationResult;
+	}
+	
+	public ArrayList formValidationGen(String form_name, ArrayList form_data) {
+
+		ArrayList validationResult = new ArrayList();
+		boolean ret = true;
+		
+		
+		
+		if (form_name.equals("pref_form")) {
+			
+			validationResult.addAll(testTextFields(form_data));
+			
+			if(validationResult.size() > 0){
+				ret = false;
+				validationResult.add(0, ret);
+			}
+			
+			/*
+			 * Commented because derby
+			 * 
+			 * if(form_data.get(3).toString().equals("")){ ret = false;
+			 * validationResult.add("Wrong username/password");
+			 * log.AddLogLine("EXCEPTION: "+form_data.get(3).toString()); }
+			 * if(form_data.get(4).toString().equals("")){ ret = false;
+			 * validationResult.add("Wrong username/password");
+			 * log.AddLogLine("EXCEPTION: "+form_data.get(4).toString()); }
+			 */
+			ReachServer reachsrv = new ReachServer(form_data.get(0).toString());
+
+			if (ret) {
+				if (reachsrv.isAlive() == false) {
+					ret = false;
+					validationResult.add("Hostname/IP address unreachable");
+					log.AddLogLine("Hostname/IP address unreachable");
+				} else {
+					DBconnector dbc = new DBconnector(form_data);
+					if (!dbc.testConn()) {
+						ret = false;
+						validationResult.add("Database connection failed");
+					} else {
+						ret = true;
+					}
+				}
+			}
+
+			if (ret && reachsrv.isAlive()) {
+
+				DBconnector dbc = new DBconnector(form_data);
+				ret = dbc.testConn();
+			}
+		}
+
+		validationResult.add(0, ret);
+
+		return validationResult;
+	}
+
+	private Logger log = new Logger(Constants.LOGNAME);
+
+	public boolean isExecuteUpdateValidArray(ArrayList<String> sqlStmts) {
+
+		boolean ret = false;
+		for (int i = 0; i < sqlStmts.size(); i++) {
+			if (!isExecuteUpdateValidString(sqlStmts.get(i))) {
+				ret = false;
+				break;
+			} else
+				ret = true;
+		}
+		return ret;
+	}
+
+	public boolean isExecuteUpdateValidString(String sql) {
+
+		boolean ret = false;
+		
+		sql = cleanSql(sql);
+		sql = getFirstCmd(sql);
+		
+		HashMap<Integer, String> hm = getValidExecuteUpdateStatements();
+		if (!hm.containsValue(sql)) ret = false; else ret = true;
+		return ret;
+	}
+
+	private HashMap<Integer, String> getValidExecuteUpdateStatements() {
+		HashMap<Integer, String> hm = new HashMap<Integer, String>();
+		hm.put(0, "update");
+		hm.put(1, "delete");
+		hm.put(2, "insert");
+		hm.put(3, "create");
+		hm.put(4, "call");
+		hm.put(5, "alter");
+		hm.put(5, "drop");
+		hm.put(6, "truncate");
+		return hm;
+	}
+	
+	private String cleanSql(String sql){
+		sql = sql.replaceAll("\n", " ");
+		while (sql.startsWith(" ")) {
+			sql = sql.substring(1);
+		}
+		log.AddLogLine("cleanSql(String sql): "+sql);
+		return sql;
+	}
+	
+	private String getFirstCmd(String sql){
+		StringTokenizer st = new StringTokenizer(sql, " ");
+		String maincmd = st.nextToken();
+		maincmd =  maincmd.toLowerCase().trim();
+		log.AddLogLine("getFirstCmd(String sql): "+maincmd);
+		return maincmd;
+	}
+}
