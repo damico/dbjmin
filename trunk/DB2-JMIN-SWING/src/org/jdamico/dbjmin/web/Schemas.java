a43808ad091c7bd947d845b92bcdb5ee62e3a0e8
jd.comment <jd.comment@1d0d8012-f3cf-11dd-802b-177316e4ac06>
2009-04-24 19:41:44 +0000

    Added first classes to web support

---
+++ trunk/DB2-JMIN-SWING/src/org/jdamico/dbjmin/web/Schemas.java
@@ -0,0 +1,17 @@
+package org.jdamico.dbjmin.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Schemas extends HttpServlet {
+
+	private static final long serialVersionUID = -8232210101653665253L;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+		
+}
