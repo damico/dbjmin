88ea263256b85133c5ad62e806d0fdb839332f05
jd.comment <jd.comment@1d0d8012-f3cf-11dd-802b-177316e4ac06>
2009-04-30 15:04:44 +0000

    Added logic to show schemas thru browser

--- trunk/DB2-JMIN-SWING/src/org/jdamico/dbjmin/web/Schemas.java
+++ trunk/DB2-JMIN-SWING/src/org/jdamico/dbjmin/web/Schemas.java
@@ -1,16 +1,34 @@
 package org.jdamico.dbjmin.web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.jdamico.dbjmin.web.action.ActionsFactory;
+
+import db2jmin.pojo.util.Constants;
+
 public class Schemas extends HttpServlet {
 
 	private static final long serialVersionUID = -8232210101653665253L;
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		ArrayList<String> schemas = ActionsFactory.callJetty(null, Constants.W_SCHEMA_ACTION).getResult();
+		PrintWriter out = response.getWriter();
+		
+		out.println(Constants.HTML_TOP);
+		out.println("<table border = '1' width = '300'>");
+		out.println("<tr bgcolor='#CCCCCC'><td><b>Schemas ("+schemas.size()+")</b></td></tr>\n");
+		for(int i=0; i<schemas.size(); i++){
+			out.println("<tr><td><a href='/tables?s="+schemas.get(i)+"'>"+schemas.get(i)+"</a></td></tr>\n");
+		}
+		out.println("</table>");
+		out.println(Constants.HTML_BOTTON);
 		
 	}
 		
