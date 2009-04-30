0723ee3de1a540f51d1a9b6e29344e748aeb67c5
jd.comment <jd.comment@1d0d8012-f3cf-11dd-802b-177316e4ac06>
2009-04-30 15:05:14 +0000

    Added logic to show tables thru browser

--- DB2-JMIN-SWING/src/org/jdamico/dbjmin/web/Tables.java
+++ DB2-JMIN-SWING/src/org/jdamico/dbjmin/web/Tables.java
@@ -1,16 +1,36 @@
 package org.jdamico.dbjmin.web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.jdamico.dbjmin.web.action.ActionsFactory;
+
+import db2jmin.pojo.util.Constants;
+
 public class Tables extends HttpServlet {
 
 	private static final long serialVersionUID = -8232210101653665253L;
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		ArrayList<String> form_data = new ArrayList<String>();
+		form_data.add(request.getParameter("s"));
+		ArrayList<String> tables = ActionsFactory.callJetty(form_data, Constants.W_TABLE_ACTION).getResult();
+		PrintWriter out = response.getWriter();
+		
+		out.println(Constants.HTML_TOP);
+		out.println("<table border = '1' width = '300'>");
+		out.println("<tr bgcolor='#CCCCCC'><td><b>Tables ("+tables.size()+")</b></td></tr>\n");
+		for(int i=0; i<tables.size(); i++){
+			out.println("<tr><td><a href='/table?t="+tables.get(i)+"'>"+tables.get(i)+"</a></td></tr>\n");
+		}
+		out.println("</table>");
+		out.println(Constants.HTML_BOTTON);
 		
 	}
 		
