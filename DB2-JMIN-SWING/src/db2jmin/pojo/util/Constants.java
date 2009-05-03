7906fa6650b26c13a5d299afb363de07c9dce983
jd.comment <jd.comment@1d0d8012-f3cf-11dd-802b-177316e4ac06>
2009-05-03 21:34:22 +0000

    Added several new constants to support Web actions

--- DB2-JMIN-SWING/src/db2jmin/pojo/util/Constants.java
+++ DB2-JMIN-SWING/src/db2jmin/pojo/util/Constants.java
@@ -27,7 +27,7 @@ package db2jmin.pojo.util;
  * @author Jose Damico (damico@dcon.com.br)
  * */
 
-public class Constants {
+public interface Constants {
 
 	public static final String APPNAME = "DBJMIN - 0.3 (2009-feb-09)";
 	public static final int LOGTEXTH = 80;
@@ -47,18 +47,54 @@ public class Constants {
 	public static final int D_SCHEMA_ACTION = 1;
 	public static final int W_SCHEMA_ACTION = 2;
 	public static final int W_TABLE_ACTION = 3;
+	public static final int W_SAMPLEDATA_ACTION = 4;
+	public static final int W_STRUCTURE_ACTION = 5;
+	public static final int W_BATCH_SQL_ACTION = 6;
+	public static final int W_SINGLE_SQL_ACTION = 7;
+	public static final int W_GET_DB_SESSION = 8;
+	public static final int W_CLOSE_SESSION = 9;
 	
 	public static final String HTML_TOP = 	"<html>\n" +
 	"<head>\n" +
-	"<title>DBJMIN: Schemas</title>\n" +
+	"<title>"+ APPNAME +"</title>\n" +
 	"</head>\n" +
-	"<body>\n" +
-	"<h2>DBJMIN: Schemas</h2><hr>";
+	"<body>";
 
 	public static final String HTML_BOTTON = "<hr>" +
 	"" + APPNAME +
 	"</body>\n" +
 	"</html>";
+	
+	public static final String HTML_SQL_FORM = 	"<form action = '/sqldata' method='post'>\n" +
+												"<table width='400'><tr><td>" +
+												"SQL:<br><textarea name='s' rows='15' cols='45'></textarea><br>\n" +
+												"</td></tr>" +
+												"<tr><td>" +
+												"<input type='submit' value='Submit' />" +
+												"</td></tr></table>" +
+												"</form>";
+	
+	public static final String HTML_SCHEMA_HEADER = "<h2>Schemas</h2><hr>\n";
+	public static final String HTML_TABLE_HEADER = "<h2>Tables</h2><hr>\n";
+	public static final String HTML_SAMPLEDATA_HEADER = "<h2>Sample Data</h2><hr>\n";
+	public static final String HTML_STRUCTURE_HEADER = "<h2>Table Structure</h2><hr>\n";
+	public static final String HTML_SQL_HEADER = "<h2>SQL</h2><hr>\n";
+	
+	public static final String HTML_TOOLS = "<table width='795'>" +
+											"<tr valign='top'>" +
+											"<td width='10'><form action = '/schemas' method='get'><input type='submit' value='Schemas' /></form></td>" +
+											"<td width='10'><form action = '/sql' method='get'><input type='submit' value='SQL' /></form></td>" +
+											"<td width='775'><center>@</center></td>" +
+											"<td width='10'><form action = '/destroy' method='get'><input type='submit' value='Close Session' /></form></td>" +
+											"</tr>" +
+											"</table>";
+	public static final int SELECT_LIMIT = 100;
+	public static final String HTML_DESTROY_HEADER = "<h2>Close Session</h2><hr>\n";
+
+	
+	
+	
+	
 	
 
 }
