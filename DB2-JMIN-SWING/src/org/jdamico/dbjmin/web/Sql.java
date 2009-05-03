efab442b04b19644130d234d5bef8f55e43215f2
jd.comment <jd.comment@1d0d8012-f3cf-11dd-802b-177316e4ac06>
2009-05-03 21:55:52 +0000

    Added several common improvements for servlets: Standards for header/footer, actions call and code cleanup

--- DB2-JMIN-SWING/src/org/jdamico/dbjmin/web/Sql.java
+++ DB2-JMIN-SWING/src/org/jdamico/dbjmin/web/Sql.java
@@ -1,17 +1,27 @@
 package org.jdamico.dbjmin.web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import db2jmin.pojo.util.Constants;
+
 public class Sql extends HttpServlet {
 
 	private static final long serialVersionUID = -8232210101653665253L;
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println(Constants.HTML_TOP);
+		out.println(Constants.HTML_SQL_HEADER);
+		out.println(Constants.HTML_TOOLS.replaceAll("@", DbSessionInfo.getInstance().getCurrentDBinfo()));
 		
+		out.println(Constants.HTML_SQL_FORM);
+		out.println(Constants.HTML_BOTTON);
+		out.close();
 	}
 		
 }
