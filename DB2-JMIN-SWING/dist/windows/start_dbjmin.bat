315f527ead9564bd7b5dfff1af9bc34103c4b3b8
jd.comment <jd.comment@1d0d8012-f3cf-11dd-802b-177316e4ac06>
2009-05-13 14:05:46 +0000

    added structure for windows dbjmin package

--- DB2-JMIN-SWING/dist/windows/start_dbjmin.bat
+++ DB2-JMIN-SWING/dist/windows/start_dbjmin.bat
@@ -1,3 +1,3 @@
-java -classpath lib\;lib\db2jcc.jar;lib\db2jcc_license_cu.jar;app\;app\db2jmin.pojo.jar;lib\mysql-connector-java-5.0.3-bin.jar;lib\ojdbc14.jar;lib\derby.jar;lib\derbyclient.jar;lib\postgresql-8.1-407.jdbc3.jar;lib\jaybird-full-2.1.0.jar; db2jmin.pojo.swing.Launch 
+java -classpath lib\;lib\db2jcc.jar;lib\db2jcc_license_cu.jar;app\;app\db2jmin.pojo.jar;lib\mysql-connector-java-5.0.3-bin.jar;lib\ojdbc14.jar;lib\derby.jar;lib\derbyclient.jar;lib\postgresql-8.1-407.jdbc3.jar;lib\jaybird-full-2.1.0.jar;lib\jetty-6.1.0.jar;lib\jetty-embedded-6.1.5.jar;lib\jetty-util-6.1.0.jar;lib\servlet-api-2.5.jar; db2jmin.pojo.swing.Launch 
 exit
 
