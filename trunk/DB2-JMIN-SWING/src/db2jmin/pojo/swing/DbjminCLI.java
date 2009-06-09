d3bb6b706a2084e22a692b58d414e677eb4f8125
jd.comment <jd.comment@1d0d8012-f3cf-11dd-802b-177316e4ac06>
2009-06-09 01:45:56 +0000

    Added class to handle CLI calls

---
+++ trunk/DB2-JMIN-SWING/src/db2jmin/pojo/swing/DbjminCLI.java
@@ -0,0 +1,22 @@
+package db2jmin.pojo.swing;
+
+import db2jmin.pojo.util.InputDataValidation;
+
+public class DbjminCLI {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		if(args[0]!=null && !args[0].trim().equals("")){
+			
+			
+			InputDataValidation idv = new InputDataValidation();
+			
+			
+		}
+
+	}
+
+}
